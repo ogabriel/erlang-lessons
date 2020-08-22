e178ed5182cfc96efa611cb4cb42e9245cf64da3
ogabriel <gabrieloliver8991@gmail.com>
2020-08-22 19:04:25 -0300

    add more types

--- types.erl
+++ types.erl
@@ -5,7 +5,9 @@ integer() ->
 	Int = 10,
 	IntBinary = 2#1010,
 	IntOcta = 8#12,
-	IntHexa = 16#A.
+	IntHexa = 16#A,
+	IntAbs = -3.14,
+	IntFloat = float(3).
 
 float() -> 1.25.
 
@@ -32,6 +34,13 @@ list() ->
 	lists:filter(fun(N) -> N > 2 end, List),
 	% [2, 4, 6, 8]
 	lists:map(fun(N) -> N * 2 end, List),
-
+	% [1, 2, 4, 4, 5]
+	lists:append(List, [5]),
+	% 2
+	lists:nth(2, List),
+	% [3, 4]
+	lists:nthtail(2, List),
+	% 4
+	lists:last(List).
 
 map() -> #{name => gabriel, age => 21}.
